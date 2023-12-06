9292b20f11ecfc10d643f8a610e3a3e5ccf3b16e
Osama <osama.assassins@gmail.com>
2023-12-06 13:16:49 +0300

    all file paths system independant now

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ const app = express();
 const port = 3000;
 app.use(morgan("tiny"));
 
-app.get("/", (req, res) => { res.sendFile(join(__dirname, '..', 'index.html')); });
-app.get("/index.js", (req, res) => { res.sendFile(join(__dirname, '..', 'index.js')); });
-app.get("/survey.html", (req, res) => { res.sendFile("/home/osama/akin-mulakat/survey-creator-project-akin/frontend/survey.html"); });
-app.get("/survey-creator.html", (req, res) => { res.sendFile("/home/osama/akin-mulakat/survey-creator-project-akin/frontend/survey-creator.html"); });
+app.get("/", (req, res) => { res.sendFile(join(__dirname, '..', 'frontend', 'index.html')); });
+app.get("/index.js", (req, res) => { res.sendFile(join(__dirname, '..', 'frontend', 'index.js')); });
+app.get("/survey.html", (req, res) => { res.sendFile(join(__dirname, '..', 'frontend', 'survey.html')); });
+app.get("/survey-creator.html", (req, res) => { res.sendFile(join(__dirname, '..', 'frontend', 'survey-creator.html')); });
 
 app.listen(port, () => { console.log(`Started server on port ${port}`); });
