e110c78931067da6e2454ed662a18f6deea11afd
osama lenovo laptop windows <osama.hamada.work@gmail.com>
2023-12-06 13:12:50 +0300

    use path and url modules for file path handling

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express"
 import morgan from "morgan"
+import { dirname, join } from "path";
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 app.use(morgan("tiny"));
 
-app.get("/", (req, res) => { res.sendFile("/home/osama/akin-mulakat/survey-creator-project-akin/frontend/index.html"); });
-app.get("/index.js", (req, res) => { res.sendFile("/home/osama/akin-mulakat/survey-creator-project-akin/frontend/index.js"); });
+app.get("/", (req, res) => { res.sendFile(join(__dirname, '..', 'index.html')); });
+app.get("/index.js", (req, res) => { res.sendFile(join(__dirname, '..', 'index.js')); });
 app.get("/survey.html", (req, res) => { res.sendFile("/home/osama/akin-mulakat/survey-creator-project-akin/frontend/survey.html"); });
 app.get("/survey-creator.html", (req, res) => { res.sendFile("/home/osama/akin-mulakat/survey-creator-project-akin/frontend/survey-creator.html"); });
 
